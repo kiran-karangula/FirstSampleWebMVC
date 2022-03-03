d70c4acc674b792dc74c5e3088e9a275519f1be4
kirankarangula <karangula.net@gmail.com>
2022-03-03 12:24:22 +0530

    Initial Commit

---
+++ DMS/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMS.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult UserInfo()
+        {
+            User user = new User()
+            {
+                UserId = 1001,
+                UserName = "Khaja Moiz",
+                DepartmentName = "Manufacturing"
+            };
+            return View(user);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
